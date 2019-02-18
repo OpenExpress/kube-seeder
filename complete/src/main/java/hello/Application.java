aae3d1100e05e7c911877bd507d0a983467bc098
Justin Mathew <justinmamathew@capgemini.com>
2019-02-18 16:26:24 +0530

    String changed

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
     @RequestMapping("/")
     public String home() {
-        return "Hello Docker World";
+        return "This is a spring boot app deployed in Kubernetes by Justin Mathew";
     }
 
     public static void main(String[] args) {
